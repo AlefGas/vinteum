e62c632bdb12552cd62675ffac8e09e7381776a9
AlefGas <lafitealef@gmail.com>
2024-02-06 21:01:12 -0300

    jogovinteum

---
+++ src/main/java/com/example/Computador.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Computador extends Jogador {
+    @Override
+    public boolean Parou() {
+         return this.getPontos() > 16 ;
+
+
+        }
+        
+}
+
+
+
